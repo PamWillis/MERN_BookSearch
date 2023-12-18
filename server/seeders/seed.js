3f14780fd55df884aa621c42bf3ba3202555dd1a
Pam <pwillisop@yahoo.com>
2023-12-17 19:32:29 -0500

    started seed files

---
+++ server/seeders/seed.js
@@ -0,0 +1,31 @@
+const db = require('../config/connection');
+const { User, Book } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const bookSeeds = require('./thoughtSeeds.json');
+const cleanDB = require('./cleanDB');
+
+db.once('open', async () => {
+  try {
+    await cleanDB('Book', 'books');
+    await cleanDB('User', 'users');
+    await User.create(userSeeds);
+    
+    for (let i = 0; i < bookSeeds.length; i++) {
+      const { _id, thoughtAuthor } = await Book.create(bookSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        {
+          $addToSet: {
+            thoughts: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
