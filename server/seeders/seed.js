8fd17bcabc670f23ce1e2ca7bf750c61a81bccf7
Pam <pwillisop@yahoo.com>
2023-12-17 19:54:16 -0500

    adjusted seed

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,26 +1,26 @@
 const db = require('../config/connection');
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const bookSeeds = require('./thoughtSeeds.json');
+// const bookSeeds = require('./thoughtSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
-    await cleanDB('Book', 'books');
+    // await cleanDB('Book', 'books');
     await cleanDB('User', 'users');
     await User.create(userSeeds);
     
-    for (let i = 0; i < bookSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Book.create(bookSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
+    // for (let i = 0; i < bookSeeds.length; i++) {
+    //   const { _id, thoughtAuthor } = await Book.create(bookSeeds[i]);
+    //   const user = await User.findOneAndUpdate(
+    //     { username: thoughtAuthor },
+    //     {
+    //       $addToSet: {
+    //         thoughts: _id,
+    //       },
+    //     }
+    //   );
+    // }
   } catch (err) {
     console.error(err);
     process.exit(1);
